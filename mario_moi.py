592dbb1746d5d832de2833cb336844be9a8edd74
AUDICTHEPROGRAMMER <audicxu@gmail.com>
2020-06-07 12:57:04 +0200

    apres avoir changer a en a[0] et aa1 en a[1]+L

--- mario_moi.py
+++ mario_moi.py
@@ -11,35 +11,47 @@ black=(0,0,0)
 block=(178,34,34)
 x=210
 y=310
+h=80
+l=20
+
 
 down=0
-z=0.01
-h=y
-a=250
-b=280
-aa=310
-bb=320
+v=0.01 # vitesse de gravite
+vv=0.1 # vitesse de descente accelere
+z=v
 
-a1=150
-b1=320
-aa1=210
-bb1=360
+hh=y
+#a=250
+#b=280
+#aa=310
+#bb=320
+#
+a=[250,150]#
+b=[280,320]#
+L=60
+H=20
+#
+#a1=150
+#b1=320
+#aa1=210
+#bb1=360
 
 
 while True:
-	up=y-bb
-	up1=int(y)-int(bb1)
-	xx=x+20
-	yy=y+80
+
+	up=y-b[0]+H
+	up1=int(y)-int(b[1]+H)
+	xx=x+l
+	yy=y+h
 	display.fill(black)
 	pygame.draw.rect(display,white,(0,0,500,10))
 	pygame.draw.rect(display,white,(0,390,500,10))
 	pygame.draw.rect(display,white,(0,0,10,400))
 	pygame.draw.rect(display,white,(490,0,10,400))
-	pygame.draw.rect(display,block,(a,b,59,20))
-	pygame.draw.rect(display,block,(a1,b1,59,20))
-	pygame.draw.rect(display,blue,(x,y+30,19,50))
-	pygame.draw.rect(display,red,(x,y,19,30))
+	pygame.draw.rect(display,block,(a[0],b[0],59,20))
+	pygame.draw.rect(display,block,(a[1],b[1],59,20))
+	pygame.draw.rect(display,blue,(x,y+30,l-1,h-30))
+	pygame.draw.rect(display,red,(x,y,l-1,h-50))
 
 
 	if x>=470:
@@ -53,21 +65,21 @@ while True:
 
 
 
-	if xx<=aa and x>=a and int(yy)==b or xx<=aa1 and x>=a1 and int(yy)==b1:
+	if x+l<=a[0]+L and x>=a[0] and int(y+h)==b or x+L<=a[1] and x>=a[1] and int(y+h)==b[1]:
 		down=2
-	elif int(yy)==b and xx>aa or int(yy)==b and x<a:
+	elif int(y+h)==b[0] and x+l>a[0]+L or int(y+h)==b[0] and x<a[0]:
 		if down==1:
 			down=1
 		else:
 			down=0
 	if down==1:
-		z=0.1
-		if y==h:
+		z=vv
+		if y==hh:
 			down=0
 	elif down==2:
 		z=0
 	elif down==0:
-		z=0.01	
+		z=v	
 	y=y+z
 
 	for event in pygame.event.get():
@@ -77,28 +89,34 @@ while True:
 		if event.type== pygame.KEYDOWN:
 
 			if event.key==pygame.K_LEFT:
-				if x==aa and x>=aa and b>y and b<=yy or x==aa1 and x>=aa1 and b1>y and b1<=yy:
+				if x==a[0]+L and x>=a[0]+L and b[0]>y and b[0]<=y+h or x==a[1]+L and x>=a[1]+L and b[1]>y and b[1]<=y+h:
 					x=x
 				else:
-					x=x-20
+					x=x-l
 			elif event.key==pygame.K_RIGHT:
-				if xx<=aa and xx==a and b>y and b<=yy or xx<=aa1 and xx==a1 and b1>y and b1<=yy:
+				if x+l<=a[0]+L and x+l==a[0] and b[0]>y and b[0]<=y+h or x+l<=a[1]+L and x+l==a[1] and b[1]>y and b[1]<=y+h:
 						x=x
 				else:
-					x=x+20
+					x=x+l
 			elif event.key==pygame.K_UP:
-				if y<=bb and y>b and xx<=aa and xx>=a and x>=a and x<=aa or y<=bb1 and y>b1 and xx<=aa1 and xx>=a1:
+				if y<=b[0]+H and y>b[0] and x+l<=a[0]+L and x+l>=a[0] and x>=a[0] and x<=a[0]+L or y<=b[1]+H and y>b[1] and x+l<=a[1]+L and x+l>=a[1]:
 					y=y
 				else:
 					down=0
-					if x>=aa or xx<=a1 or xx<=a and x>=aa1 or up>=60 and x<aa and x>a or up>=60 and xx<aa and xx>a or up1>=60 and x>a1 and x<aa1 or up1>=60 and xx>a1 and xx<aa1 or y<=b or y<=b1:
-# condition3xx<=a and x>=aa1, l'obstable0 doit etre a droite de l'obstacle1
+					if 
+
+
+
+
+
+
+
 						y=y-60
 
-					if up<60 and up>0 and x<aa and xx>a:
+					if up<60 and up>0 and x<a[0]+L and x+l>a[0]:
 						print("a0 "+str(up))
 						y=y-up
-					if up<60 and up1>0 and x<aa1 and xx>a1:
+					if up<60 and up1>0 and x<a[1]+L and x+l>a[1]:
 						print("a1"+str(up1))
 						y=y-up1
 					
