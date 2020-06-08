c8903cf33a711782e5b7a66bfd8f1b153a23d178
AUDICTHEPROGRAMMER <audicxu@gmail.com>
2020-06-08 19:51:02 +0200

    asdsadsad

--- mario_moi.py
+++ mario_moi.py
@@ -2,7 +2,7 @@ import pygame,sys
 from pygame.locals import*
 pygame.init()
 
-display=pygame.display.set_mode((1500,400))
+display=pygame.display.set_mode((500,400))
 #redgreenblue
 white=(255,255,255)
 blue=(0,0,255)
@@ -14,10 +14,10 @@ y=310
 h=80
 l=20
 
-
+upup=60 #hauteur du jump
 down=0
 v=0.01 # vitesse de gravite
-vv=0.1 # vitesse de descente accelere
+vv=0.2 # vitesse de descente accelere
 z=v
 
 hh=y
@@ -26,8 +26,8 @@ hh=y
 #aa=310
 #bb=320
 #
-a=[250,150]#
-b=[280,320]#
+a=[150,250]#
+b=[280,280]#b=320
 L=60
 H=20
 #
@@ -35,12 +35,15 @@ H=20
 #b1=320
 #aa1=210
 #bb1=360
+ok=0
+okright=0
 
 
 while True:
-
-	up=y-b[0]+H
-	up1=int(y)-int(b[1]+H)
+	
+	up=[int(y-b[0]-H),int(y-int(b[1])-H)]
+	#up=y-b[0]-H
+	#up1=int(y)-int(b[1]-H)
 	xx=x+l
 	yy=y+h
 	display.fill(black)
@@ -76,9 +79,13 @@ while True:
 		z=vv
 		if y==hh:
 			down=0
-	elif down==2:
+
+	for ww in range(len(b)):
+		if int(y)+h==b[ww] and a[ww]<=x and a[ww]+L>=x+l:
+			down=2
+	if down==2:
 		z=0
-	elif down==0:
+	if down==0:
 		z=v	
 	y=y+z
 
@@ -89,36 +96,42 @@ while True:
 		if event.type== pygame.KEYDOWN:
 
 			if event.key==pygame.K_LEFT:
-				if x==a[0]+L and x>=a[0]+L and b[0]>y and b[0]<=y+h or x==a[1]+L and x>=a[1]+L and b[1]>y and b[1]<=y+h:
+				if x==a[0]+L and b[0]>y and b[0]<=y+h or x==a[0]+L and b[0]+H>y and b[0]+H<=y+h or x==a[1]+L and x>=a[1]+L and b[1]>y and b[1]<=y+h or x==a[1]+L and x>=a[1]+L and b[1]+H>y and b[1]+H<=y+h:
 					x=x
 				else:
 					x=x-l
+
 			elif event.key==pygame.K_RIGHT:
-				if x+l<=a[0]+L and x+l==a[0] and b[0]>y and b[0]<=y+h or x+l<=a[1]+L and x+l==a[1] and b[1]>y and b[1]<=y+h:
-						x=x
+				if x+l<=a[0]+L and x+l==a[0] and b[0]>y and b[0]<=y+h or x+l<=a[0]+L and x+l==a[0] and b[0]+H>y and b[0]+H<=y+h or x+l<=a[1]+L and x+l==a[1] and b[1]>y and b[1]<=y+h or x+l<=a[1]+L and x+l==a[1] and b[1]+H>y and b[1]+H<=y+h:
+					x=x
 				else:
 					x=x+l
+	
+					
+
+
 			elif event.key==pygame.K_UP:
-				if y<=b[0]+H and y>b[0] and x+l<=a[0]+L and x+l>=a[0] and x>=a[0] and x<=a[0]+L or y<=b[1]+H and y>b[1] and x+l<=a[1]+L and x+l>=a[1]:
-					y=y
-				else:
-					down=0
-					if 
+				if len(a)==1:
+					if x<a[0] or x+l>a[0]+L or up[0]>upup or y+h>=b[0]:
+						y=y-upup
+						down=0
+				if len(a)>=2:
+					if x+l<=a[0]or x>=a[len(a)-1]+L:
+						y=y-upup
+						down=0
+						ok=1
+					for w in range(len(a)-1):
+						if x>=a[w]+L and x+l<=a[w+1] or up[w+1]<0 or up[0]<0:
+							if ok==0:
+								y=y-upup
+								down=0
+							elif ok==1:
+								ok=0
 
-
-
-
-
-
-
-						y=y-60
-
-					if up<60 and up>0 and x<a[0]+L and x+l>a[0]:
-						print("a0 "+str(up))
-						y=y-up
-					if up<60 and up1>0 and x<a[1]+L and x+l>a[1]:
-						print("a1"+str(up1))
-						y=y-up1
+					if up[0]<upup and up[0]>0 and x<a[0]+L and x+l>a[0]:
+						y=y-up[0]
+					if up[0]<upup and up[1]>0 and x<a[1]+L and x+l>a[1]:
+						y=y-up[1]
 					
 			elif event.key==pygame.K_DOWN:
 				down=1
