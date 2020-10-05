50d177cdbed0ccd498e8945f7dd1e79687321b66
AUDICTHEPROGRAMMER <audicxu@gmail.com>
2020-10-05 17:05:06 +0200

    second degree equation

--- 2-2.py
+++ 2-2.py
@@ -1,6 +1,6 @@
 a=int(input())
 b=int(input())
-c=int(input()))
+c=int(input())
 
 if a==b==c==0:
   print("inf")
@@ -9,7 +9,7 @@ elif a==b==0:
 elif a==0 and b!=0:
   print("1")
 else:
-  delta=b*b-4(a*c)
+  delta=b*b-4*(a*c)
   if delta>0:
     print("2")
   if delta==0:
